b363d9e1a6c2df80dc0e7ed93f5aaf61d82994c4
christ-saragih <bennefit.19@gmail.com>
2024-07-28 19:16:58 +0700

    CREATED: Komponen movie card

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,4 +1,5 @@
 import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import { Head } from "@inertiajs/react";
 import Flickity from "react-flickity-component";
@@ -20,6 +21,7 @@ const Dashboard = () => {
                     rel="stylesheet"
                     href="https://unpkg.com/flickity@2/dist/flickity.min.css"
                 />
+                <title>Dashboard</title>
             </Head>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">
@@ -40,6 +42,28 @@ const Dashboard = () => {
                         />
                     ))}
                 </Flickity>
+
+                {/* Browse */}
+                <div className="mt-12">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+
+                    <Flickity
+                        options={flickityOptions}
+                        className="gap-[30px] __scroll-selector"
+                    >
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MovieCard
+                                key={i}
+                                slug={"the-batman-in-love"}
+                                name={`The Batman in Love ${i}`}
+                                category={"Comedy"}
+                                thumbnail={"/assets/images/browse-1.png"}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
             </div>
         </Authenticated>
     );
