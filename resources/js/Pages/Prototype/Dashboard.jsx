a330c00cf1a3e6fed7635c51b380ab95435eb8c8
christ-saragih <bennefit.19@gmail.com>
2024-07-28 18:56:22 +0700

    CREATED: Komponen featured movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,10 +1,47 @@
+import FeaturedMovie from "@/Components/FeaturedMovie";
 import Authenticated from "@/Layouts/Authenticated/Index";
+import { Head } from "@inertiajs/react";
+import Flickity from "react-flickity-component";
 
 const Dashboard = () => {
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
     return (
-        <div>
-            <Authenticated />
-        </div>
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity
+                    options={flickityOptions}
+                    className="gap-[30px] __scroll-selector"
+                >
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeaturedMovie
+                            key={i}
+                            slug={"the-batman-in-love"}
+                            name={`The Batman in Love ${i}`}
+                            category={"Comedy"}
+                            thumbnail={"/assets/images/featured-1.png"}
+                            rating={i + 1}
+                        />
+                    ))}
+                </Flickity>
+            </div>
+        </Authenticated>
     );
 };
 
